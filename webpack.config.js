2c23abc4804d2ba119b4b4524df338a85f4004de
Kevin <yupolin921021@gmail.com>
2020-01-11 11:05:46 -0800

    Added HTMLWebpackPlugin to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.join(__dirname, "public/index.html"),
+  filename: "index.html"
+});
+
 module.exports = {
   entry: __dirname + "/src/index.js",
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         use: {
-          loader: "babel-loader",
-          exclude: /node_modules/
-        }
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
       }
     ]
   },
   output: {
     filename: "bundle.js",
-    path: __dirname + "/build"
-  }
+    path: path.join(__dirname, 'dist'),
+  },
+  plugins: [HTMLWebpackPluginConfig]
 };
